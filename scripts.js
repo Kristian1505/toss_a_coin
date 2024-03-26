a0a6f2483117adc53dd93ac33cd7f6ccf549d9b2
Kristian Hernandez <hernandez.cardenas10@gmail.com>
2024-03-26 02:38:44 -0600

    Descriptions updated

--- scripts.js
+++ scripts.js
@@ -7,14 +7,28 @@ const getTrueorFalse = (randomNumber) => randomNumber < 0.5;
 // Returns "Cara" if trueOrFalse is true, otherwise returns "Cruz". This function decides the outcome based on the boolean input.
 const getCaraCruz = (trueOrFalse) => trueOrFalse ? "Cara" : "Cruz";
 
-// Triggers the coin flip process by generating a random number, evaluating it, and updating the innerHTML of the element with id "pp" to show "Cara" or "Cruz".
-const flipCoin = () => document.getElementById("message").innerHTML = getCaraCruz(getTrueorFalse(getRandomNumber()));
+// Initiates the coin flip process, evaluates it, and updates the innerHTML of the element with id "message" to display "Cara" or "Cruz".
+const flipCoin = () => {
+    const result = getCaraCruz(getTrueorFalse(getRandomNumber()));
+    document.getElementById("message").innerHTML = result;
+    return result
+}
+
+// Retrieves the image element from the document for future manipulation.
+const webImage = document.getElementById("image")
 
 // Changes the text of the button with id "buttonFlipCoin" to "Volver a lanzar" after a click event, indicating the coin has been flipped and offering a prompt for another flip.
 const changeMessage = () => document.getElementById("buttonFlipCoin").innerText = "Volver a lanzar"
 
-// Attaches an event listener to the button with id "buttonFlipCoin" that triggers the flipCoin function, initiating the coin flip process when the button is clicked.
-document.getElementById("buttonFlipCoin").addEventListener("click", flipCoin);
+// Adjusts the source of the image based on the coin flip result and ensures the image is visible.
+const changeImage = (result) => {
+    webImage.src = result === "Cara" ? "./img/head_cat.png" : "./img/tail_cat.png";
+    webImage.style.visibility = "visible";
+}
 
-// Attaches another event listener to the same button that triggers the changeMessage function, changing the button's text to indicate another action can be taken.
-document.getElementById("buttonFlipCoin").addEventListener("click", changeMessage);
+// Attaches click event listeners to the "buttonFlipCoin" button to trigger the coin flip, update the button text, and change the display image accordingly.
+document.getElementById("buttonFlipCoin").addEventListener("click", () =>{
+    const result = flipCoin();
+    changeMessage();
+    changeImage(result)
+})
